bb846cba2ef917a00d2be18d2c7e427338d9df59
Aiden Petersen <AidenPetersen@protonmail.com>
2022-06-06 20:15:38 -0500

    Reorganized packages

--- nixos/modules/graphical/themes.nix
+++ nixos/modules/graphical/themes.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 let 
-  adw-gtk3 = pkgs.callPackage ../nix/pkgs/adw-gtk3 { };
+  adw-gtk3 = pkgs.callPackage ../packages/adw-gtk3 { };
 in {
   environment.systemPackages = [
     adw-gtk3
